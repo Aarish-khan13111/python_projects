83e5327f374491d91357b327e951e1e3b046bdb5
Aarish khan <aarishkhan13111@gmail.com>
2022-09-05 15:32:52 +0530

    trend of production over the years

--- netflixdata analysis/netflix.py
+++ netflixdata analysis/netflix.py
@@ -29,3 +29,25 @@ directorsTop5=directorsTop5.sort_values(by=['Total Content'])
 fig1=px.bar(directorsTop5,x='Total Content',y='Director',title='Top 5 Directors on Netflix')
 fig1.show()
 
+#Now see Top 5 Actors
+df['cast']=df['cast'].fillna('No Cast Specified')
+filtered_cast=pd.DataFrame()
+filtered_cast=df['cast'].str.split(',',expand=True).stack()
+filtered_cast=filtered_cast.to_frame()
+filtered_cast.columns=['Actor']
+actors=filtered_cast.groupby(['Actor']).size().reset_index(name='Total Content')
+actors=actors[actors.Actor !='No Cast Specified']
+actors=actors.sort_values(by=['Total Content'],ascending=False)
+actorsTop5=actors.head()
+actorsTop5=actorsTop5.sort_values(by=['Total Content'])
+fig2=px.bar(actorsTop5,x='Total Content',y='Actor', title='Top 5 Actors on Netflix')
+fig2.show()
+
+#Next thing is to analyze from this data is the trend of production over the years on netflix
+df1=df[['type','release_year']]
+df1=df1.rename(columns={"release_year": "Release year"})
+df2=df1.groupby(['Release year','type']).size().reset_index(name='Total content')
+df2=df2[df2['Release year']>=2010]
+fig3 = px.line(df2,x="Release year",y="Total content",color='type',title='Trend on content produce over the years on Netfilx')
+fig3.show()
+
